f8a82d1df65074fd8302116cc95050bdbb5e358d
shen <978622005@qq.com>
2020-03-25 22:56:41 +0800

    注入Java对象，取出Java对象。

--- app/src/androidTest/java/com/sjianjun/reader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sjianjun/reader/ExampleInstrumentedTest.kt
@@ -2,12 +2,18 @@ package com.sjianjun.reader
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.sjianjun.reader.bean.ATest
 import com.sjianjun.reader.http.HttpInterface
 import com.sjianjun.reader.http.createRetrofit
 import kotlinx.coroutines.runBlocking
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.ScriptableObject
+import sjj.alog.Log
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,7 +32,7 @@ class ExampleInstrumentedTest {
     @Test
     fun testGet() {
         val get = get("https://www.biquge5200.cc/95_95192/")
-        log(get)
+        Log.e(get)
     }
 
     @Test
@@ -34,6 +40,48 @@ class ExampleInstrumentedTest {
         val get = post("https://log.kxxsc.com/yd/exceptionlog/report")
         log(get)
     }
+
+    @Test
+    fun testJava2Js() {
+        val context = Context.enter()
+        //android 使用 Dalvik 所以不能优化为class 字节码
+        context.optimizationLevel = -1
+        val scriptable = context.initStandardObjects()
+
+        val a = ATest()
+        val jsA = Context.javaToJS(a, scriptable)
+        ScriptableObject.putProperty(scriptable, "aaa", jsA)
+
+        val evaluateString = context.evaluateString(scriptable, """
+                    aaa.test()
+                """.trimIndent(), null, 0, null)
+        Log.e(evaluateString)
+        Log.e(Context.toString(evaluateString))
+        Context.exit()
+    }
+
+    @Test
+    fun testHtmlDom() {
+        val get = get("https://www.biquge5200.cc/95_95192/")
+        val doc = Jsoup.parse(get)
+
+
+        val context = Context.enter()
+        //android 使用 Dalvik 所以不能优化为class 字节码
+        context.optimizationLevel = -1
+        val scriptable = context.initStandardObjects()
+
+        val jsDoc = Context.javaToJS(doc, scriptable)
+        ScriptableObject.putProperty(scriptable, "doc", jsDoc)
+
+        val evaluateString = context.evaluateString(scriptable, """
+                    doc.getElementById("info")
+                """.trimIndent(), null, 0, null)
+        Log.e(Context.jsToJava(evaluateString, Element::class.java))
+        Log.e(Context.toString(evaluateString))
+        Context.exit()
+    }
+
 
     fun get(
         url: String = "",
