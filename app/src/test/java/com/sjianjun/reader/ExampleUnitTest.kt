d1bb392a262c2f51bc884d66baa92319927610c1
shenjianjun <978622005@qq.com>
2020-03-25 16:58:41 +0800

    init

---
+++ app/src/test/java/com/sjianjun/reader/ExampleUnitTest.kt
@@ -0,0 +1,23 @@
+package com.sjianjun.reader
+
+import org.jsoup.Jsoup
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        println("hello test addition_isCorrect")
+        assertEquals(4, 2 + 2)
+    }
+
+    fun loadHtml(url: String) {
+
+    }
+}
