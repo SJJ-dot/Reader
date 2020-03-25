9f5c429f32801fcd6f13744eca5b7103cfcaa75b
shenjianjun <978622005@qq.com>
2020-03-25 18:17:54 +0800

    get post test

--- app/src/androidTest/java/com/sjianjun/reader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sjianjun/reader/ExampleInstrumentedTest.kt
@@ -1,12 +1,13 @@
 package com.sjianjun.reader
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.sjianjun.reader.http.HttpInterface
+import com.sjianjun.reader.http.createRetrofit
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +21,37 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.sjianjun.reader", appContext.packageName)
+    }
+
+    @Test
+    fun testGet() {
+        val get = get("https://www.biquge5200.cc/95_95192/")
+        log(get)
+    }
+
+    @Test
+    fun testPost() {
+        val get = post("https://log.kxxsc.com/yd/exceptionlog/report")
+        log(get)
+    }
+
+    fun get(
+        url: String = "",
+        header: Map<String, String> = emptyMap(),
+        queryMap: Map<String, String> = emptyMap()
+    ) = runBlocking {
+        createRetrofit().create(HttpInterface::class.java).get(url, header, queryMap).await()
+    }
+
+    fun post(
+        url: String = "",
+        header: Map<String, String> = emptyMap(),
+        fieldMap: Map<String, String> = emptyMap()
+    ) = runBlocking {
+        createRetrofit().create(HttpInterface::class.java).post(url, header, fieldMap).await()
+    }
+
+    private fun log(msg: String) {
+        println("ExampleInstrumentedTest $msg")
     }
 }
--- app/src/main/java/com/sjianjun/reader/http/HttpInterface.kt
+++ app/src/main/java/com/sjianjun/reader/http/HttpInterface.kt
@@ -6,10 +6,10 @@ import retrofit2.http.*
 interface HttpInterface {
 
     @GET
-    fun getRequest(@Url url: String = "", @HeaderMap header: Map<String, String> = emptyMap(), @QueryMap queryMap: Map<String, String> = emptyMap()): Deferred<String>
+    fun get(@Url url: String = "", @HeaderMap header: Map<String, String> = emptyMap(), @QueryMap queryMap: Map<String, String> = emptyMap()): Deferred<String>
 
 
     @FormUrlEncoded
     @POST
-    fun postRequest(@Url url: String = "", @HeaderMap header: Map<String, String> = emptyMap(), @FieldMap fieldMap: Map<String, String> = emptyMap())
+    fun post(@Url url: String = "", @HeaderMap header: Map<String, String> = emptyMap(), @FieldMap fieldMap: Map<String, String> = emptyMap()): Deferred<String>
 }
