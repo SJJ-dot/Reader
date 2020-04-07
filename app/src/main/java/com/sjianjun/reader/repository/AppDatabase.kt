fca2b6a21518f12f6e4c4602853eb460dcb74044
shen <978622005@qq.com>
2020-04-08 07:47:28 +0800

    ..

--- app/src/main/java/com/sjianjun/reader/repository/AppDatabase.kt
+++ app/src/main/java/com/sjianjun/reader/repository/AppDatabase.kt
@@ -5,12 +5,12 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sjianjun.reader.App
 import com.sjianjun.reader.bean.*
-import com.sjianjun.reader.utils.withIo
 import com.sjianjun.reader.utils.withSingle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
 @Database(
     entities = [Book::class, JavaScript::class, SearchHistory::class, Chapter::class, ReadingRecord::class],
@@ -20,12 +20,21 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun dao(): Dao
 }
 
+private val threadId = AtomicInteger(0)
+private val transaction = Executors.newFixedThreadPool(1) { r ->
+    Thread(r, String.format("room_io_%d", threadId.getAndIncrement()))
+}
+
 val db = Room.databaseBuilder(App.app, AppDatabase::class.java, "app_database")
+//    .setQueryExecutor(transaction)
+//    .setTransactionExecutor(transaction)
     .build()
 
 
-fun <T> transaction(block: suspend CoroutineScope.() -> T): T {
-    return db.runInTransaction(Callable {
-        runBlocking(block = block)
-    })
+suspend fun <T> transaction(block: suspend CoroutineScope.() -> T): T {
+    return withSingle {
+        db.runInTransaction(Callable {
+            runBlocking(block = block)
+        })
+    }
 }
