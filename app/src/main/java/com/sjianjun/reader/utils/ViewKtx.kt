8961bd1d1577c7274378a68f7156c3ae5d2ec843
shen <978622005@qq.com>
2020-04-12 16:14:14 +0800

    未读章节数

---
+++ app/src/main/java/com/sjianjun/reader/utils/ViewKtx.kt
@@ -0,0 +1,36 @@
+package com.sjianjun.reader.utils
+
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.sjianjun.reader.R
+
+fun View.show() {
+    visibility(View.VISIBLE, this)
+}
+
+fun View.hide() {
+    visibility(View.INVISIBLE, this)
+}
+
+fun View.gone() {
+    visibility(View.GONE, this)
+}
+
+fun visibility(visibility: Int, vararg viewList: View) {
+    val view = viewList.firstOrNull() ?: return
+    val parent = view.parent
+    if (parent is ConstraintLayout) {
+        var tag = view.getTag(R.id.visibility) as? ConstraintSet
+        if (tag == null) {
+            tag = ConstraintSet()
+            view.setTag(R.id.visibility, tag)
+        }
+        tag.clone(parent)
+        viewList.forEach {
+            tag.setVisibility(it.id, visibility)
+        }
+        tag.applyTo(parent)
+    }
+    view.visibility = visibility
+}
