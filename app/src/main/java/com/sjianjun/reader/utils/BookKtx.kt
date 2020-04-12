80eaea43044f1cbff5119d4ee4ed19365e368a01
shen <978622005@qq.com>
2020-04-12 14:08:24 +0800

    版本更新检查

--- app/src/main/java/com/sjianjun/reader/utils/BookKtx.kt
+++ app/src/main/java/com/sjianjun/reader/utils/BookKtx.kt
@@ -22,3 +22,28 @@ fun List<SearchResult>.toBookGroup(map: MutableMap<String, MutableList<SearchRes
 fun key(bookTitle: String, bookAuthor: String): String {
     return "title:${bookTitle} author:${bookAuthor}"
 }
+
+
+/**
+ * 获取章节名。eg:第1章 序章 返回“序章”
+ */
+fun Chapter.name(): String {
+    val title = title?.trim() ?: return ""
+    if (title.isEmpty()) {
+        return title
+    }
+    listOf(
+        "^第[0-9[一二三四五六七八九零十百千万]]+[章节回](.+$)",
+        "^第[0-9[一二三四五六七八九零十百千万]]+(.+$)",
+        "^[0-9[一二三四五六七八九零十百千万]]+[章节回](.+$)",
+        "^[0-9[一二三四五六七八九零十百千万]]+(.+$)"
+    ).map(Pattern::compile)
+        .forEach {
+            val matcher = it.matcher(title)
+            if (matcher.find()) {
+                return matcher.group(1) ?: title
+            }
+        }
+    Log.e("matcher failed chapter Name:$title")
+    return title
+}
