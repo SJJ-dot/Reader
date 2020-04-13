af8c32a3a68229894b9c92d659133936e287f1b2
shenjianjun <978622005@qq.com>
2020-04-13 18:35:02 +0800

    ...

--- app/src/main/java/com/sjianjun/reader/module/update/CheckUdate.kt
+++ app/src/main/java/com/sjianjun/reader/module/update/CheckUdate.kt
@@ -16,12 +16,26 @@ import sjj.novel.util.fromJson
 import sjj.novel.util.gson
 
 suspend fun checkUpdate(activity: BaseActivity) = withIo {
+    val header = mapOf(
+        "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" to "identity",
+        "Accept-Language" to "zh-CN,zh;q=0.9,en;q=0.8",
+        "Cache-Control" to "no-cache",
+        "Connection" to "keep-alive",
+        "Host" to "api.github.com",
+        "Pragma" to "no-cache",
+        "Sec-Fetch-Dest" to "document",
+        "Sec-Fetch-Mode" to "navigate",
+        "Sec-Fetch-Site" to "none",
+        "Sec-Fetch-User" to "?1",
+        "Upgrade-Insecure-Requests" to "1"
+    )
     val info =
-        http.get("https://api.github.com/repos/SJJ-dot/Reader/releases/latest")
+        http.get("https://api.github.com/repos/SJJ-dot/Reader/releases/latest", header = header)
     if (info.isNotEmpty()) {
         globalConfig.releasesInfo = info
     }
-    val githubApi = gson.fromJson<GithubApi>(globalConfig.releasesInfo)?:return@withIo null
+    val githubApi = gson.fromJson<GithubApi>(globalConfig.releasesInfo) ?: return@withIo null
 
     val download = githubApi.assets?.find { it.content_type == CONTENT_TYPE_ANDROID }
     if (download?.browser_download_url.isNullOrEmpty()) {
