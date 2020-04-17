bbecd25c1264511fb71cfeef0a87647fa7d8c83f
shenjianjun <978622005@qq.com>
2020-04-17 16:43:06 +0800

    左滑删除bug

--- app/src/main/java/com/sjianjun/reader/utils/ChapterKtx.kt
+++ app/src/main/java/com/sjianjun/reader/utils/ChapterKtx.kt
@@ -1,31 +1,7 @@
 package com.sjianjun.reader.utils
 
-import com.sjianjun.reader.bean.Book
 import com.sjianjun.reader.bean.Chapter
-import com.sjianjun.reader.bean.SearchResult
-import sjj.alog.Log
 import java.util.regex.Pattern
-
-fun List<Book>?.toGroup(map: MutableMap<String, MutableList<Book>> = mutableMapOf()): MutableMap<String, MutableList<Book>> {
-    this?.forEach {
-        val list = map.getOrPut(key(it.title, it.author)) { mutableListOf() }
-        list.add(it)
-    }
-    return map
-}
-
-fun List<SearchResult>.toBookGroup(map: MutableMap<String, MutableList<SearchResult>> = mutableMapOf()) {
-    forEach {
-        val list = map.getOrPut(key(it.bookTitle, it.bookAuthor)) { mutableListOf() }
-        list.add(it)
-    }
-}
-
-
-fun key(bookTitle: String, bookAuthor: String): String {
-    return "title:${bookTitle} author:${bookAuthor}"
-}
-
 
 /**
  * 获取章节名。eg:第1章 序章 返回“序章”
