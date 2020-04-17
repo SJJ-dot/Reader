4424e68c8645d338e22eca9bc2f9e66915d9753c
shen <978622005@qq.com>
2020-04-17 21:21:00 +0800

    夜间模式按钮

--- app/src/main/java/com/sjianjun/reader/App.kt
+++ app/src/main/java/com/sjianjun/reader/App.kt
@@ -1,12 +1,17 @@
 package com.sjianjun.reader
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.sjianjun.reader.preferences.globalConfig
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         app = this
         handleDefaultException(this)
+
+        AppCompatDelegate.setDefaultNightMode(globalConfig.appDayNightMode)
+
     }
 
 
--- app/src/main/java/com/sjianjun/reader/BaseActivity.kt
+++ app/src/main/java/com/sjianjun/reader/BaseActivity.kt
@@ -2,18 +2,14 @@ package com.sjianjun.reader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.lifecycle.coroutineScope
 import com.gyf.immersionbar.ImmersionBar
 import com.sjianjun.reader.utils.handler
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 abstract class BaseActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
         ImmersionBar.with(this).init()
     }
