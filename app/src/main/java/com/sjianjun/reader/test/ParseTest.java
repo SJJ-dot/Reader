8dc8ebe522765ea7e4150f1472ba3ea40c75e081
shenjianjun <978622005@qq.com>
2020-04-17 15:06:39 +0800

    天籁小说

--- app/src/main/java/com/sjianjun/reader/test/ParseTest.java
+++ app/src/main/java/com/sjianjun/reader/test/ParseTest.java
@@ -21,75 +21,55 @@ import java.util.function.Function;
 import sjj.alog.Log;
 
 public final class ParseTest {
-    private static String source = "起点中文网";
+    private static String source = "天籁小说";
 
     public static List<SearchResult> search(Http http, String query) throws Exception {
-        List<SearchResult> searchResults = new ArrayList<>();
-        String baseUrl = "https://www.qidian.com/";
-        String html = http.get(baseUrl + "search?kw=" + URLEncoder.encode(query, "utf-8"));
+        List<SearchResult> results = new ArrayList<>();
+        String baseUrl = "https://www.23txt.com/";
+        String html = http.get(baseUrl + "search.php?keyword=" + URLEncoder.encode(query, "utf-8"));
         Document parse = Jsoup.parse(html, baseUrl);
-        Elements bookElItemList = parse.select(".res-book-item");
-        Element bookElItm = bookElItemList.get(0);
-        SearchResult result = new SearchResult();
-        result.source = source;
-        result.bookTitle = bookElItm.select(".book-mid-info h4").text();
-        result.bookUrl = bookElItm.select(".book-mid-info h4 a").get(0).absUrl("href");
-        result.bookAuthor = bookElItm.select(".author a").get(0).text();
-        result.latestChapter = bookElItm.select(".update a").get(0).text();
-        result.bookCover = bookElItm.select("img").get(0).absUrl("src");
-        searchResults.add(result);
-        return searchResults;
+        Elements bookListEl = parse.select("body > div.result-list > *");
+        for (int i = 0; i < bookListEl.size(); i++) {
+            Element bookEl = bookListEl.get(i);
+            SearchResult result = new SearchResult();
+            result.source = source;
+            result.bookTitle = bookEl.select("a.result-game-item-title-link").text();
+            result.bookUrl = bookEl.select("a.result-game-item-title-link").get(0).absUrl("href");
+            result.bookAuthor = bookEl.select("> div.result-game-item-detail > div > p:nth-child(1) > span:nth-child(2)").get(0).text();
+            result.latestChapter = bookEl.select("> div.result-game-item-detail > div > p:nth-child(4) > a").get(0).text();
+            result.bookCover = bookEl.select("img.result-game-item-pic-link-img").get(0).absUrl("src");
+            results.add(result);
+        }
+        return results;
     }
 
     public static Book getBookDetails(Http http, String url) {
-        Book book = new Book();
-        book.url = url;
-        book.source = source;
         Document parse = Jsoup.parse(http.get(url), url);
-        Element bookInfoEl = parse.select(".book-info").get(0);
-        book.title = bookInfoEl.select("h1 em").text();
-        book.author = bookInfoEl.select("h1 span a").text();
-        book.intro = parse.select(".book-intro").text();
-        book.cover = parse.select("#bookImg > img").get(0).absUrl("src");
+        Book book = new Book();
+        book.source = source;
+        book.url = url;
+        book.title = parse.select("#info > h1").text();
+        book.author = parse.select("#info > p:nth-child(2)").text().split("者：")[1];
+        book.intro = parse.select("#intro").text();
+        book.cover = parse.select("#fmimg > img").get(0).absUrl("src");
         List<Chapter> chapterList = new ArrayList<>();
 
-        Elements chapterListEl = parse.select(".volume-wrap > .volume > .cf li a");
-        if (chapterListEl.isEmpty()) {
-            String bookId = bookInfoEl.select("#addBookBtn").attr("data-bookid");
-            String chapterListUrl = "https://m.qidian.com/book/" + bookId + "/catalog";
-            String chapterListHtml = http.get("https://m.qidian.com/book/" + bookId + "/catalog");
-            Elements phoneChapterListEl = Jsoup.parse(chapterListHtml,chapterListUrl).select(".chapter-li-a");
-            for (int i = 0; i < phoneChapterListEl.size(); i++) {
-                Element chapterEl = phoneChapterListEl.get(i);
-                Chapter chapter = new Chapter();
-                chapter.bookUrl = book.url;
-                chapter.title = chapterEl.select("span").text();
-                chapter.url = chapterEl.absUrl("href");
-                chapterList.add(chapter);
-            }
-
-        } else {
-            for (int i = 0; i < chapterListEl.size(); i++) {
-                Element chapterEl = chapterListEl.get(i);
-                Chapter chapter = new Chapter();
-                chapter.bookUrl = book.url;
-                chapter.title = chapterEl.text();
-                chapter.url = chapterEl.absUrl("href");
-                chapterList.add(chapter);
-            }
+        Elements chapterListEl = parse.select("#list > dl a");
+        for (int i = 0; i < chapterListEl.size(); i++) {
+            Element chapterEl = chapterListEl.get(i);
+            Chapter chapter = new Chapter();
+            chapter.bookUrl = book.url;
+            chapter.title = chapterEl.text();
+            chapter.url = chapterEl.absUrl("href");
+            chapterList.add(chapter);
         }
         book.chapterList = chapterList;
         return book;
     }
 
     public static String getBookChapterContent(Http http, String url) {
-
         String html = http.get(url);
-        if (url.startsWith("https://m.qidian.com/")) {
-            return Jsoup.parse(html).select("#chapterContent").html();
-        } else {
-            return Jsoup.parse(html).select(".main-text-wrap  div.read-content").html();
-        }
+        return Jsoup.parse(html).select("#content").html();
     }
 
     public static <R> R js(Function<ContextWrap, R> function) {
@@ -97,7 +77,7 @@ public final class ParseTest {
     }
 
     public static void test() throws Exception {
-        List<SearchResult> searchResults = search(HttpKt.getHttp(), "诡秘之主");
+        List<SearchResult> searchResults = search(HttpKt.getHttp(), "哈利波特");
         Log.e(searchResults);
         if (searchResults.isEmpty()) {
             return;
