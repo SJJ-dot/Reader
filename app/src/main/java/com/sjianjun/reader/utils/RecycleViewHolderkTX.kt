986a0a5d73299dfc5d8afd3136c78800e4bf0766
shen <978622005@qq.com>
2020-04-17 23:04:22 +0800

    .

--- app/src/main/java/com/sjianjun/reader/utils/RecycleViewHolderkTX.kt
+++ app/src/main/java/com/sjianjun/reader/utils/RecycleViewHolderkTX.kt
@@ -1,11 +1,14 @@
 package com.sjianjun.reader.utils
 
 import androidx.recyclerview.widget.RecyclerView
+import com.sjianjun.reader.R
 
 fun RecyclerView.ViewHolder.setRecyclable(recyclable: Boolean) {
-    if (isRecyclable && !recyclable) {
-        setIsRecyclable(recyclable)
-    } else if (!isRecyclable && recyclable){
-        setIsRecyclable(recyclable)
+    if (itemView.getTag(R.id.recyclable) != false && !recyclable) {
+        setIsRecyclable(false)
+        itemView.setTag(R.id.recyclable, false)
+    } else if (itemView.getTag(R.id.recyclable) == false && recyclable) {
+        itemView.setTag(R.id.recyclable, true)
+        setIsRecyclable(true)
     }
 }
