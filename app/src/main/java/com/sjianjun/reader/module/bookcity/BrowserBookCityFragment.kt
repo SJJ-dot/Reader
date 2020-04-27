d65ec95640b6f8b8d259ab861ce0b9da784704ac
shenjianjun <978622005@qq.com>
2020-04-27 17:03:14 +0800

    。

---
+++ app/src/main/java/com/sjianjun/reader/module/bookcity/BrowserBookCityFragment.kt
@@ -0,0 +1,10 @@
+package com.sjianjun.reader.module.bookcity
+
+import com.sjianjun.reader.BaseFragment
+import com.sjianjun.reader.R
+
+class BrowserBookCityFragment : BaseFragment() {
+    override fun getLayoutRes(): Int {
+        return R.layout.bookcity_fragment_browser
+    }
+}
