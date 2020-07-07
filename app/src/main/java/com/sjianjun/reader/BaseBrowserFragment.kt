f8753748f5696d9950152ed786d88f6a13a659c9
shenjianjun <978622005@qq.com>
2020-07-07 14:21:25 +0800

    hostUrl

--- app/src/main/java/com/sjianjun/reader/BaseBrowserFragment.kt
+++ app/src/main/java/com/sjianjun/reader/BaseBrowserFragment.kt
@@ -3,6 +3,7 @@ package com.sjianjun.reader
 import android.app.DownloadManager
 import android.net.Uri
 import android.os.Environment
+import android.view.View.SCROLLBARS_OUTSIDE_OVERLAY
 import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -19,15 +20,8 @@ abstract class BaseBrowserFragment : BaseAsyncFragment() {
         WebView.setWebContentsDebuggingEnabled(true)
 //声明WebSettings子类
         val webSettings = webView.getSettings();
-        webSettings.userAgentString =
-            "Mozilla/5.0 (Linux; Android 9; MIX 2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.0 Mobile Safari/537.36 EdgA/44.11.2.4122"
-//如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
-        webSettings.javaScriptEnabled = true
-// 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
-// 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
 
-//支持插件
-//        webSettings.setPluginsEnabled(true);
+        webSettings.javaScriptEnabled = true
 
 //设置自适应屏幕，两者合用
         webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
@@ -35,17 +29,10 @@ abstract class BaseBrowserFragment : BaseAsyncFragment() {
 
 //缩放操作
         webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
-        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
-        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
-
-//其他细节操作
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT); //关闭webview中缓存
-        webSettings.setAllowFileAccess(true); //设置可以访问文件
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
-        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
-        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-
+        webSettings.builtInZoomControls = true; //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.displayZoomControls = false; //隐藏原生的缩放控件
+        webView.scrollBarStyle = SCROLLBARS_OUTSIDE_OVERLAY
+        webView.isScrollbarFadingEnabled = false
         webView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
             val uri = Uri.parse(url)
             activity?.also {
