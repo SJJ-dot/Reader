674862403b43877fd96a797c3d7b4d452247b4fd
shenjianjun <978622005@qq.com>
2020-07-13 10:40:15 +0800

    Slop

--- app/src/main/java/com/sjianjun/reader/view/HideBottomViewOnScrollBehavior2.kt
+++ app/src/main/java/com/sjianjun/reader/view/HideBottomViewOnScrollBehavior2.kt
@@ -3,8 +3,10 @@ package com.sjianjun.reader.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
+import kotlin.math.abs
 
 /*
  * Created by shen jian jun on 2020-07-10
@@ -17,6 +19,8 @@ class HideBottomViewOnScrollBehavior2<V : View> : HideBottomViewOnScrollBehavior
         return super.onLayoutChild(parent, child, layoutDirection)
     }
 
+    var dySum = 0
+
     override fun onNestedScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
@@ -28,17 +32,29 @@ class HideBottomViewOnScrollBehavior2<V : View> : HideBottomViewOnScrollBehavior
         type: Int,
         consumed: IntArray
     ) {
+        val dy = if (dyConsumed == 0) dyUnconsumed else dyConsumed
+        dySum += abs(dy)
 
         super.onNestedScroll(
             coordinatorLayout,
             child,
             target,
             dxConsumed,
-            if (dyConsumed == 0) dyUnconsumed else dyConsumed,
+            if (abs(dySum) > ViewConfiguration.get(coordinatorLayout.context).scaledTouchSlop) dy else 0,
             dxUnconsumed,
             dyUnconsumed,
             type,
             consumed
         )
+    }
+
+    override fun onStopNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        type: Int
+    ) {
+        super.onStopNestedScroll(coordinatorLayout, child, target, type)
+        dySum = 0
     }
 }
