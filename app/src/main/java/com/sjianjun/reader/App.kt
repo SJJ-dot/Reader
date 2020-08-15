70dd8220be5706890d9ed04891f3619017726933
shen <978622005@qq.com>
2020-08-15 22:30:17 +0800

    LeakCanary

--- app/src/main/java/com/sjianjun/reader/App.kt
+++ app/src/main/java/com/sjianjun/reader/App.kt
@@ -4,18 +4,24 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.sjianjun.reader.preferences.globalConfig
 import com.sjianjun.reader.utils.ActivityManger
+import leakcanary.LeakCanaryProcess
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanaryProcess.isInAnalyzerProcess(this)) {
+            //LeakCanary 分析内存堆转储文件时，单独启动一个进程，尽量避免影响主进程
+            return
+        }
         app = this
         handleDefaultException(this)
         ActivityManger.init(this)
         AppCompatDelegate.setDefaultNightMode(globalConfig.appDayNightMode)
+
     }
 
 
     companion object {
-        lateinit var app:App
+        lateinit var app: App
     }
 }
