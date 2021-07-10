4b832b8e4164899dc451c92ca57a967004d2f172
shen <978622005@qq.com>
2021-07-10 14:49:56 +0800

    删除 Cockroach

--- app/src/main/java/com/sjianjun/reader/DefaultUncaughtExceptionHandler.kt
+++ app/src/main/java/com/sjianjun/reader/DefaultUncaughtExceptionHandler.kt
@@ -12,27 +12,27 @@ fun handleDefaultException(context: Context) {
 //    CrashReport.initCrashReport(context, "d3d6da5bd7", BuildConfig.DEBUG);
     //检查应用更新 崩溃上报
     Bugly.init(context,"d3d6da5bd7",BuildConfig.DEBUG)
-    val default = Thread.getDefaultUncaughtExceptionHandler()
-    Cockroach.install(context, object : ExceptionHandler() {
-        override fun onUncaughtExceptionHappened(thread: Thread?, throwable: Throwable?) {
-            Log.e("捕获到异常 ", Exception(throwable))
-            CrashReport.postCatchedException(throwable, thread)
-        }
-
-        override fun onBandageExceptionHappened(throwable: Throwable?) {
-            Log.e("捕获到异常 ", Exception(throwable))
-            CrashReport.postCatchedException(throwable)
-        }
-
-        override fun onEnterSafeMode() {
-            Log.e("进入安全模式")
-        }
-
-        override fun onMayBeBlackScreen(throwable: Throwable) {
-            Log.e("捕获到异常 ", Exception(throwable))
-//            CrashReport.postCatchedException(e)
-            default?.uncaughtException(Thread.currentThread(), throwable)
-        }
-    })
+//    val default = Thread.getDefaultUncaughtExceptionHandler()
+//    Cockroach.install(context, object : ExceptionHandler() {
+//        override fun onUncaughtExceptionHappened(thread: Thread?, throwable: Throwable?) {
+//            Log.e("捕获到异常 ", Exception(throwable))
+//            CrashReport.postCatchedException(throwable, thread)
+//        }
+//
+//        override fun onBandageExceptionHappened(throwable: Throwable?) {
+//            Log.e("捕获到异常 ", Exception(throwable))
+//            CrashReport.postCatchedException(throwable)
+//        }
+//
+//        override fun onEnterSafeMode() {
+//            Log.e("进入安全模式")
+//        }
+//
+//        override fun onMayBeBlackScreen(throwable: Throwable) {
+//            Log.e("捕获到异常 ", Exception(throwable))
+////            CrashReport.postCatchedException(e)
+//            default?.uncaughtException(Thread.currentThread(), throwable)
+//        }
+//    })
 
 }
