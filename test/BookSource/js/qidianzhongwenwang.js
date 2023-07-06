f370b0963c1770930dbd1727107de9b94a7b9db5
shen <978622005@QQ.COM>
2023-07-06 19:00:55 +0800

    书源BUG修复

--- test/BookSource/js/qidianzhongwenwang.js
+++ test/BookSource/js/qidianzhongwenwang.js
@@ -1,62 +1,52 @@
-function search(query){
+function search(query) {
     var baseUrl = "https://www.qidian.com/";
-    var url = "soushu/" + URLEncoder.encode(query, "utf-8")+".html";
-    var doc = get({baseUrl:baseUrl,url:url})
+    var url = "soushu/" + URLEncoder.encode(query, "utf-8") + ".html";
+    var doc = get({baseUrl: baseUrl, url: url})
 
     var bookListEl = doc.select(".res-book-item");
     var results = new ArrayList();
-    for (var i=0;i<bookListEl.size();i++){
+    for (var i = 0; i < bookListEl.size(); i++) {
         var bookEl = bookListEl.get(i);
         var result = new SearchResult();
         result.bookTitle = bookEl.select(".book-info-title").get(0).select("a").get(0).text();
-        result.bookUrl = "https://m.qidian.com/book/"+(bookEl.select("a[data-bid]").attr("data-bid"));
+        result.bookUrl = "https://m.qidian.com/book/" + (bookEl.select("a[data-bid]").attr("data-bid"));
         result.bookAuthor = bookEl.select(".author").select(".author > :nth-child(2)").get(0).text();
         results.add(result);
     }
     return results;
 }
 
-function getDetails(url){
-    var doc = get({url:url});
+function getDetails(url) {
+    var doc = get({url: url});
     var book = new Book();
     book.url = url;
+    Log.e(">>>>>book.title")
     book.title = doc.select("meta[property=\"og:novel:book_name\"]").attr("content");
+    Log.e(book.bookTitle)
     book.author = doc.select("meta[property=\"og:novel:author\"]").attr("content");
+    Log.e(book.author)
     book.intro = doc.select("meta[property=\"og:description\"]").attr("content");
+    Log.e(book.intro)
     book.cover = doc.select("meta[property=\"og:image\"]").attr("content");
+    Log.e(book.cover)
     var chapterListUrl = doc.select("#details-menu").get(0).absUrl("href");
-    var docChapterList = get({url:chapterListUrl});
-    var elements = docChapterList.select("script");
+    var chapterListEl = get({url:chapterListUrl}).select(".y-list__content a");
+    Log.e(chapterListEl.size())
     var chapterList = new ArrayList();
-    var bookId = url.match(/\d+/)
-    for (i = 0; i < elements.size(); i++) {
-        var data = elements.get(i).data();
-        if (data.contains("g_data.volumes")) {
-            try{
-                data = eval(data+"\n"+"g_data.volumes");
-                Log.e(data)
-                for (i = 0; i < data.length; i++) {
-                    var chapterListJson = data[i]["cs"]
-                    for (j = 0; j < chapterListJson.length; j++) {
-                        var chapterJson = chapterListJson[j];
-                        var chapter = new Chapter();
-                        chapter.title = chapterJson["cN"];
-                        chapter.url = "https://vipreader.qidian.com/chapter/"+bookId+"/"+chapterJson["id"];
-                        chapterList.add(chapter);
-                    }
-                }
-                break;
-            }catch(error){
-                Log.e(source+"解析章节列表出错，"+error)
-                break;
-            }
-        }
+    for (i = 0; i < chapterListEl.size(); i++) {
+        var chapterEl = chapterListEl.get(i);
+        var chapter = new Chapter();
+        chapter.title = chapterEl.text();
+        Log.e(chapter.title)
+        chapter.url = chapterEl.absUrl("href");
+        Log.e(chapter.url)
+        chapterList.add(chapter);
     }
     book.chapterList = chapterList;
     return book;
 }
 
-function getChapterContent(url){
-    var doc = get({url:url});
-    return doc.select(".read-content").html()
+function getChapterContent(url) {
+    var doc = get({url: url});
+    return doc.select("main").html()
 }
--- test/src/main/java/com/sjianjun/test/test.js
+++ test/src/main/java/com/sjianjun/test/test.js
@@ -1,95 +1,52 @@
-function search(query){
-    var baseUrl = "https://www.69shu.com/modules/article/search.php";
-    var map = new HashMap();
-    map.put("searchtype", "all")
-    map.put("searchkey",URLEncoder.encode(query,"gbk"))
-    var resp = http.post(baseUrl,map);
-    var parse = Jsoup.parse(resp.body, resp.url);
-    if(baseUrl == resp.url){
-        var bookListEl = parse.select(".newbox > ul > li");
-        Log.e(">>>>>bookListEl Size "+bookListEl.size()+">>>>>>>>>>>>>>>>>>>>")
-        var results = new ArrayList();
-        for (var i=0;i<bookListEl.size();i++){
-            var bookEl = bookListEl.get(i);
-            Log.e(bookEl.tagName()+">>>>>"+i+">>>>>>>>>>>>>>>>>>>>")
-            Log.e("bookEl>>>>")
-            Log.e(bookEl)
-            var result = new SearchResult();
-            Log.e("bookTitle>>>>")
-            result.bookTitle = bookEl.select("h3 a").get(0).text();
-            Log.e(result.bookTitle)
-            Log.e("bookUrl>>>>")
-            result.bookUrl = bookEl.select("h3 a").get(0).absUrl("href");
-            Log.e(result.bookUrl)
-            Log.e("bookAuthor>>>>")
-            result.bookAuthor = bookEl.select(".labelbox label").get(0).text();
-            Log.e(result.bookAuthor)
-            results.add(result);
-            Log.e(bookEl.tagName()+"<<<<<"+i+"<<<<<<<<<<<<<<<<<<<<")
-        }
-        return results;
-    }
-    var results = new ArrayList();
-    var bookEl = parse;
-    Log.e(bookEl.tagName()+">>>>>"+i+">>>>>>>>>>>>>>>>>>>>")
-    Log.e("bookEl>>>>")
-//    Log.e(bookEl)
-    var result = new SearchResult();
-    Log.e("bookTitle>>>>")
-    result.bookTitle = bookEl.select("meta[property=\"og:title\"]").attr("content");
-    Log.e(result.bookTitle)
-    Log.e("bookUrl>>>>")
-    result.bookUrl = bookEl.select("meta[property=\"og:novel:read_url\"]").attr("content");
-    Log.e(result.bookUrl)
-    Log.e("bookAuthor>>>>")
-    result.bookAuthor = bookEl.select("meta[property=\"og:novel:author\"]").attr("content");
-    Log.e(result.bookAuthor)
-    results.add(result);
-    return results;
+function search(query) {
+    var baseUrl = "https://www.qidian.com/";
+    var url = "soushu/" + URLEncoder.encode(query, "utf-8") + ".html";
+    var doc = get({baseUrl: baseUrl, url: url})
 
+    var bookListEl = doc.select(".res-book-item");
+    var results = new ArrayList();
+    for (var i = 0; i < bookListEl.size(); i++) {
+        var bookEl = bookListEl.get(i);
+        var result = new SearchResult();
+        result.bookTitle = bookEl.select(".book-info-title").get(0).select("a").get(0).text();
+        result.bookUrl = "https://m.qidian.com/book/" + (bookEl.select("a[data-bid]").attr("data-bid"));
+        result.bookAuthor = bookEl.select(".author").select(".author > :nth-child(2)").get(0).text();
+        results.add(result);
+    }
+    return results;
 }
 
-/**
- * 书籍详情[JavaScript.source]
- */
-function getDetails(url){
-    var parse = Jsoup.parse(http.get(url).body,url);
+function getDetails(url) {
+    var doc = get({url: url});
     var book = new Book();
-    book.source = source;
     book.url = url;
-    Log.e("title>>>>")
-    book.title =  parse.selectFirst("[property=og:title]").attr("content");
-    Log.e(book.title)
-    Log.e("author>>>>")
-    book.author = parse.selectFirst("[property=og:novel:author]").attr("content");
+    Log.e(">>>>>book.title")
+    book.title = doc.select("meta[property=\"og:novel:book_name\"]").attr("content");
+    Log.e(book.bookTitle)
+    book.author = doc.select("meta[property=\"og:novel:author\"]").attr("content");
     Log.e(book.author)
-    Log.e("intro>>>>")
-    book.intro = parse.selectFirst("[property=og:description]").attr("content");
+    book.intro = doc.select("meta[property=\"og:description\"]").attr("content");
     Log.e(book.intro)
-    Log.e("cover>>>>")
-    book.cover = parse.selectFirst("[property=og:image]").attr("content");
+    book.cover = doc.select("meta[property=\"og:image\"]").attr("content");
     Log.e(book.cover)
-    //加载章节列表
-    var chapterUrl = url.replace("txt/","").replace(".htm","/")
-    var children = Jsoup.parse(http.get(chapterUrl).body,chapterUrl).select("#catalog ul > li a");
-    Log.e("章节列表数量："+children.size())
+    var chapterListUrl = doc.select("#details-menu").get(0).absUrl("href");
+    var chapterListEl = get({url:chapterListUrl}).select(".y-list__content a");
+    Log.e(chapterListEl.size())
     var chapterList = new ArrayList();
-    for(i=0; i<children.size(); i++){
-        var chapterEl = children.get(i);
+    for (i = 0; i < chapterListEl.size(); i++) {
+        var chapterEl = chapterListEl.get(i);
         var chapter = new Chapter();
-        Log.e("chapter.title>>>>")
         chapter.title = chapterEl.text();
         Log.e(chapter.title)
-        Log.e("chapter.url>>>>")
         chapter.url = chapterEl.absUrl("href");
-        Log.e(chapter.title)
+        Log.e(chapter.url)
         chapterList.add(chapter);
     }
     book.chapterList = chapterList;
     return book;
 }
 
-function getChapterContent(url){
-    var html = http.get(url).body;
-    return Jsoup.parse(html).selectFirst(".txtnav").outerHtml();
+function getChapterContent(url) {
+    var doc = get({url: url});
+    return doc.select("main").html()
 }
