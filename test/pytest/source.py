a9f7a89bffee7b864265b11c2767f0afb8bada58
shen <978622005@qq.com>
2024-02-19 16:57:17 +0800

    文趣阁

--- test/pytest/source.py
+++ test/pytest/source.py
@@ -185,12 +185,3 @@ def getChapterContent(chapter_url):
     # 章节内容 html
     content = soup["content"]
     return content.replace("\n", "<br>")
-
-
-if __name__ == '__main__':
-    result = search("我的")
-    log(result)
-    result = getDetails(result[0]["bookUrl"])
-    log(result)
-    result = getChapterContent(result["chapterList"][0]["url"])
-    log(result)
