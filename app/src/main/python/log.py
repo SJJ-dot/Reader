e13be842470dfd4b8036864d66d01921d0fb3aaa
shen <978622005@qq.com>
2024-11-21 12:36:19 +0800

    log.py

--- app/src/main/python/log.py
+++ app/src/main/python/log.py
@@ -3,7 +3,7 @@ from com.sjianjun.reader.utils import PyLog
 
 from http.client import HTTPConnection
 HTTPConnection.debuglevel=1
-from requests import PreparedRequest
+from requests import PreparedRequest, Response
 
 def log(arg):
     if isinstance(arg, PreparedRequest):
@@ -11,7 +11,14 @@ def log(arg):
         for key, value in arg.headers.items():
             PyLog.i(f"{key}: {value}")
         PyLog.i("")
-        PyLog.i(arg.body)
+        PyLog.i(f"{arg.body}")
         PyLog.i("")
+    elif isinstance(arg, Response):
+        # 打印响应头
+        for key, value in arg.headers.items():
+            print(f"{key}: {value}")
+        print("")
+        print(f"{arg.text}")
+        print("")
     else:
-        PyLog.i(arg)
+        PyLog.i(f"{arg}")
