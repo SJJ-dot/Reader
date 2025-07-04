b809cb0f41fca24e6fe648489bccbd9280bc35fd
shen <978622005@qq.com>
2025-07-04 12:29:46 +0800

    BackstageWebView

--- app/src/main/java/io/legado/app/help/http/BackstageWebView.kt
+++ app/src/main/java/io/legado/app/help/http/BackstageWebView.kt
@@ -7,12 +7,13 @@ import android.os.Looper
 import android.util.AndroidRuntimeException
 import android.webkit.SslErrorHandler
 import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
 import com.sjianjun.reader.App
 import com.sjianjun.reader.WEB_VIEW_UA_ANDROID
+import com.sjianjun.reader.utils.gson
 import io.legado.app.exception.NoStackTraceException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -31,21 +32,16 @@ import kotlin.coroutines.resumeWithException
  */
 class BackstageWebView(
     private val url: String? = null,
-    private val html: String? = null,
-    private val encode: String? = null,
-    private val tag: String? = null,
     private val headerMap: Map<String, String>? = null,
-    private val sourceRegex: String? = null,
-    private val overrideUrlRegex: String? = null,
-    private val javaScript: String? = null,
-    private val delayTime: Long = 0,
+    private val javaScript: String = "document.documentElement.outerHTML",
+    private val timeout: Long = 20000L,
 ) {
 
     private val mHandler = Handler(Looper.getMainLooper())
     private var callback: Callback? = null
     private var mWebView: WebView? = null
 
-    suspend fun geResponse(act: AppCompatActivity): String = withTimeout(10000L) {
+    suspend fun getResponse(): String = withTimeout(timeout) {
         suspendCancellableCoroutine { continuation ->
             continuation.invokeOnCancellation {
                 launch(Dispatchers.Main) {
@@ -53,9 +49,9 @@ class BackstageWebView(
                 }
             }
             callback = object : Callback() {
-                override fun onResult(response: String) {
+                override fun onResult(response: WebViewResponse) {
                     if (continuation.isActive) {
-                        continuation.resume(response)
+                        continuation.resume(gson.toJson(response))
                     }
                 }
 
@@ -66,7 +62,7 @@ class BackstageWebView(
             }
             launch(Dispatchers.Main) {
                 try {
-                    load(act)
+                    load()
                 } catch (error: Throwable) {
                     continuation.resumeWithException(error)
                 }
@@ -74,22 +70,12 @@ class BackstageWebView(
         }
     }
 
-    private fun getEncoding(): String {
-        return encode ?: "utf-8"
-    }
-
     @Throws(AndroidRuntimeException::class)
-    private fun load(act: AppCompatActivity) {
-        val webView = createWebView(act)
+    private fun load() {
+        val webView = createWebView()
         mWebView = webView
         try {
             when {
-                !html.isNullOrEmpty() -> if (url.isNullOrEmpty()) {
-                    webView.loadData(html, "text/html", getEncoding())
-                } else {
-                    webView.loadDataWithBaseURL(url, html, "text/html", getEncoding(), url)
-                }
-
                 else -> if (headerMap == null) {
                     webView.loadUrl(url!!)
                 } else {
@@ -102,34 +88,21 @@ class BackstageWebView(
     }
 
     @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
-    private fun createWebView(act: AppCompatActivity): WebView {
-        val webView = WebView(act)
+    private fun createWebView(): WebView {
+        val webView = WebView(App.app)
         val settings = webView.settings
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
         settings.blockNetworkImage = true
         settings.userAgentString = headerMap?.get("User-Agent") ?: WEB_VIEW_UA_ANDROID
         settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-        if (sourceRegex.isNullOrBlank() && overrideUrlRegex.isNullOrBlank()) {
-            webView.webViewClient = HtmlWebViewClient()
-        } else {
-            webView.webViewClient = SnifferWebClient()
-        }
+        webView.webViewClient = HtmlWebViewClient()
         return webView
     }
 
     private fun destroy() {
         mWebView?.destroy()
         mWebView = null
-    }
-
-    private fun getJs(): String {
-        javaScript?.let {
-            if (it.isNotEmpty()) {
-                return it
-            }
-        }
-        return JS
     }
 
     private inner class HtmlWebViewClient : WebViewClient() {
@@ -141,16 +114,22 @@ class BackstageWebView(
             view: WebView,
             request: WebResourceRequest
         ): Boolean {
+            Log.e("==========>: shouldOverrideUrlLoading" + request.url)
             isRedirect = isRedirect || request.isRedirect
             return super.shouldOverrideUrlLoading(view, request)
         }
 
+        override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+            Log.e("==========>: shouldInterceptRequest" + request?.url)
+            return super.shouldInterceptRequest(view, request)
+        }
+
         override fun onPageFinished(view: WebView, url: String) {
             if (runnable == null) {
-                runnable = EvalJsRunnable(view, url, getJs())
+                runnable = EvalJsRunnable(view, url, javaScript)
             }
             mHandler.removeCallbacks(runnable!!)
-            mHandler.postDelayed(runnable!!, 1000 + delayTime)
+            mHandler.postDelayed(runnable!!, 1000)
         }
 
         @SuppressLint("WebViewClientOnReceivedSslError")
@@ -171,18 +150,16 @@ class BackstageWebView(
             private val mWebView: WeakReference<WebView> = WeakReference(webView)
             override fun run() {
                 mWebView.get()?.evaluateJavascript(mJavaScript) {
-                    Log.e("evaluateJavascript")
                     handleResult(it)
                 }
             }
 
             private fun handleResult(result: String) = GlobalScope.launch {
-                if (result.isNotEmpty() && result != "null") {
+                if (!result.isBlank() && result != "null") {
                     val content = StringEscapeUtils.unescapeJson(result)
                         .replace(quoteRegex, "")
                     try {
-                        val response = buildStrResponse(content)
-                        callback?.onResult(response)
+                        callback?.onResult(WebViewResponse(url, content))
                     } catch (e: Exception) {
                         callback?.onError(e)
                     }
@@ -202,101 +179,18 @@ class BackstageWebView(
                 mHandler.postDelayed(this@EvalJsRunnable, 1000)
             }
 
-            private fun buildStrResponse(content: String): String {
-                if (!isRedirect) {
-                    return content
-                }
-                return content
-            }
-        }
-
-    }
-
-    private inner class SnifferWebClient : WebViewClient() {
-
-        override fun shouldOverrideUrlLoading(
-            view: WebView,
-            request: WebResourceRequest
-        ): Boolean {
-            if (shouldOverrideUrlLoading(request.url.toString())) {
-                return true
-            }
-            return super.shouldOverrideUrlLoading(view, request)
-        }
-
-        @Suppress("DEPRECATION", "OVERRIDE_DEPRECATION", "KotlinRedundantDiagnosticSuppress")
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            if (shouldOverrideUrlLoading(url)) {
-                return true
-            }
-            return super.shouldOverrideUrlLoading(view, url)
-        }
-
-        private fun shouldOverrideUrlLoading(requestUrl: String): Boolean {
-            overrideUrlRegex?.let {
-                if (requestUrl.matches(it.toRegex())) {
-                    try {
-                        val response = requestUrl
-                        callback?.onResult(response)
-                    } catch (e: Exception) {
-                        callback?.onError(e)
-                    }
-                    destroy()
-                    return true
-                }
-            }
-            return false
-        }
-
-        override fun onLoadResource(view: WebView, resUrl: String) {
-            sourceRegex?.let {
-                if (resUrl.matches(it.toRegex())) {
-                    try {
-                        val response = resUrl
-                        callback?.onResult(response)
-                    } catch (e: Exception) {
-                        callback?.onError(e)
-                    }
-                    destroy()
-                }
-            }
-        }
-
-        override fun onPageFinished(webView: WebView, url: String) {
-            if (!javaScript.isNullOrEmpty()) {
-                val runnable = LoadJsRunnable(webView, javaScript)
-                mHandler.postDelayed(runnable, 1000L + delayTime)
-            }
-        }
-
-        @SuppressLint("WebViewClientOnReceivedSslError")
-        override fun onReceivedSslError(
-            view: WebView?,
-            handler: SslErrorHandler?,
-            error: SslError?
-        ) {
-            handler?.proceed()
-        }
-
-        private inner class LoadJsRunnable(
-            webView: WebView,
-            private val mJavaScript: String?
-        ) : Runnable {
-            private val mWebView: WeakReference<WebView> = WeakReference(webView)
-            override fun run() {
-                mWebView.get()?.loadUrl("javascript:${mJavaScript}")
-            }
         }
 
     }
 
     companion object {
-        const val JS = "document.documentElement.outerHTML"
         private val quoteRegex = "^\"|\"$".toRegex()
     }
 
     abstract class Callback {
-        abstract fun onResult(response: String)
+        abstract fun onResult(response: WebViewResponse)
         abstract fun onError(error: Throwable)
     }
+
+    class WebViewResponse(url: String, val html: String)
 }
