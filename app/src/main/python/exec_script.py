2751c4b8022914cd629a6b6880280899cb381f53
shen <978622005@qq.com>
2025-07-04 09:50:27 +0800

    json dump

--- app/src/main/python/exec_script.py
+++ app/src/main/python/exec_script.py
@@ -1,8 +1,13 @@
 import copy
-
+import json
 def exec_script(script, func, arg):
     # 1. 准备安全环境
     safe_builtins = copy.copy(__builtins__.__dict__) if hasattr(__builtins__, '__dict__') else copy.copy(__builtins__)
     namespace = {'__builtins__': safe_builtins}
     exec(script, namespace)
-    return namespace[func](arg)
+    result = namespace[func](arg)
+    if isinstance(result, dict):
+        json_str = json.dumps(result, ensure_ascii=False)
+    else:
+        json_str = result
+    return json_str
