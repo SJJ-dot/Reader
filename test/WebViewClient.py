76fc32435ebebf2419e1f16d8da319c0c2ea4e2b
shen <978622005@qq.com>
2026-05-09 18:16:14 +0800

    添加 web_post接口

--- test/WebViewClient.py
+++ test/WebViewClient.py
@@ -26,6 +26,16 @@ def web_get(url, headers=None, javaScript="document.documentElement.outerHTML", 
     response.encoding = 'utf-8'
     return response.text
 
+def web_post(url, postData=None, javaScript="document.documentElement.outerHTML", timeout=20000, encoding="UTF-8"):
+    init()
+    server_url = f'http://{_server_ip}:{_server_port}/web_get'
+    # POST请求的数据
+    data = {'url': url, 'postData': postData,'javaScript': javaScript, 'timeout': timeout, 'encoding': encoding}
+    # 发送POST请求
+    response = requests.post(server_url, json=data)
+    response.encoding = 'utf-8'
+    return response.text
+
 
 if __name__ == '__main__':
     # Example usage
