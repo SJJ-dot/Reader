fb17976689bfe8445605f43cf15b2460b54a419c
shen <978622005@qq.com>
2026-05-13 17:46:26 +0800

    阅读书源兼容

---
+++ test/yuedu.py
@@ -0,0 +1,127 @@
+import json
+
+import requests
+# 阅读3.0书源兼容脚本。
+#     source_url = "https://legado.aoaostar.com/sources/71e56d4f.json"
+#     response = json.loads(requests.get(source_url).text)
+#     for item in response:
+#         bookSourceName = item['bookSourceName']
+#         if bookSourceName != "阅友小说":
+#             continue
+#         book_source = json.dumps(item, indent=4, ensure_ascii=False)
+#         print(">>>>>>>>================================")
+#         print(book_source)
+#         print("<<<<<<<<================================")
+# 阅读书源json字符串。APP拼接阅读规则和Python模板
+yuedu_source = r"""
+{
+    "bookSourceComment": "备用地址：https://sma.yueyouxs.com",
+    "bookSourceName": "阅友小说",
+    "bookSourceType": 0,
+    "bookSourceUrl": "http://m.suixkan.com",
+    "customOrder": 24,
+    "enabled": true,
+    "enabledCookieJar": true,
+    "enabledExplore": true,
+    "exploreUrl": "[{\"title\":\"推荐\",\"url\":\"\",\"style\":{\"layout_flexBasisPercent\":1,\"layout_flexGrow\":1}},\n{\"title\":\"重磅推荐\",\"url\":\"/l/s/28/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.4,\"layout_flexGrow\":1}},\n{\"title\":\"男生必读\",\"url\":\"/l/s/29/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.4,\"layout_flexGrow\":1}},\n{\"title\":\"女生爱看\",\"url\":\"/l/s/30/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.4,\"layout_flexGrow\":1}},\n{\"title\":\"小编推荐\",\"url\":\"/l/s/31/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.4,\"layout_flexGrow\":1}},\n{\"title\":\"男频\",\"url\":\"\",\"style\":{\"layout_flexBasisPercent\":1,\"layout_flexGrow\":1}},\n{\"title\":\"都市人生\",\"url\":\"/l/f/1100/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"玄幻奇幻\",\"url\":\"/l/f/1101/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"武侠仙侠\",\"url\":\"/l/f/1102/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"军事历史\",\"url\":\"/l/f/1103/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"科幻末世\",\"url\":\"/l/f/1104/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"游戏体育\",\"url\":\"/l/f/1105/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"热血青春\",\"url\":\"/l/f/1106/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"悬疑灵异\",\"url\":\"/l/f/1107/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"脑洞大开\",\"url\":\"/l/f/1108/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"女频\",\"url\":\"\",\"style\":{\"layout_flexBasisPercent\":1,\"layout_flexGrow\":1}},\n{\"title\":\"现代言情\",\"url\":\"/l/f/2100/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"古代言情\",\"url\":\"/l/f/2101/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"幻想言情\",\"url\":\"/l/f/2102/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"\",\"url\":\"/l/f/2103/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"穿越时空\",\"url\":\"/l/f/2104/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"宫闱争斗\",\"url\":\"/l/f/2105/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"豪门总裁\",\"url\":\"/l/f/2106/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"婚恋爱情\",\"url\":\"/l/f/2107/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"经商种田\",\"url\":\"/l/f/2108/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"图书\",\"url\":\"\",\"style\":{\"layout_flexBasisPercent\":1,\"layout_flexGrow\":1}},\n{\"title\":\"出版读物\",\"url\":\"/l/f/3101/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"文学小说\",\"url\":\"/l/f/3102/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}},\n{\"title\":\"古代典籍\",\"url\":\"/l/f/3103/{{page}}.html\",\"style\":{\"layout_flexBasisPercent\":0.25,\"layout_flexGrow\":1}}]",
+    "lastUpdateTime": 1680526062517,
+    "respondTime": 1238,
+    "ruleBookInfo": {
+        "author": ".face-info span.0@text##.*：",
+        "coverUrl": ".face-cover img@src",
+        "intro": "#intro@html",
+        "kind": ".face-info span.1:3@text&&#idNewIds@#chapter-ps-id@text##.*：",
+        "lastChapter": "#idNewIds@.chapter-entrance@text",
+        "name": ".face-info-title@text",
+        "tocUrl": ".sumchapter a@href",
+        "wordCount": ".face-info span.2@text##.*："
+    },
+    "ruleContent": {
+        "content": ".con@html",
+        "replaceRegex": "##[\\(（]本章未完.*[）\\)]|[\\(（]本章完[）\\)]"
+    },
+    "ruleExplore": {
+        "author": ".v-author@text##\\s",
+        "bookList": ".v-list-item",
+        "bookUrl": "@onclick@js:result.match(/\\('(.*?)'\\)/)[1]",
+        "coverUrl": "img@src",
+        "intro": ".v-intro@text",
+        "name": ".v-title@text",
+        "wordCount": ".v-words@text"
+    },
+    "ruleSearch": {
+        "author": ".v-author@text##\\s",
+        "bookList": ".v-list-item",
+        "bookUrl": "@onclick@js:result.match(/\\('(.*?)', '', ''\\)/)[1]",
+        "coverUrl": "img@src",
+        "intro": ".v-intro@text",
+        "kind": ".base-label@text",
+        "name": ".v-title@text",
+        "wordCount": ".v-words@text"
+    },
+    "ruleToc": {
+        "chapterList": ".catalog_ls li a",
+        "chapterName": "text",
+        "chapterUrl": "href"
+    },
+    "searchUrl": "/s/1.html?keyword={{key}}&page={{page}}",
+    "weight": 0
+}
+"""
+yuedu_source = json.loads(yuedu_source.strip())
+
+
+def verify():
+    """
+    可选，部分书籍不支持搜索功能时提供，避免验证书源失败
+    验证书源是否可用，返回True或False
+    """
+    pass
+
+def getSiteUrl():
+    """
+    可选，如果不包含该函数，书源无法使用链接直接导入，也无法在书城进行导入
+    :return: 书源网址，例如：https://www.69shuba.com，用于直接导入书籍详情页链接
+    """
+    return str(yuedu_source["bookSourceUrl"])
+
+
+def search(query):
+    """
+    :param query: 搜索关键词
+    :return: 书籍列表，包含以下字段：
+        - bookTitle: 书籍标题
+        - bookUrl: 书籍详情链接
+        - bookAuthor: 书籍作者(可选)
+        - bookCover: 书籍封面链接(可选)
+
+    """
+    pass
+
+
+def getDetails(url):
+    """
+    :param url: 书籍详情链接
+    :return: 书籍详情字典，包含以下字段：
+        - url: 书籍详情链接
+        - title: 书籍标题
+        - author: 书籍作者
+        - cover: 书籍封面链接
+        - intro: 书籍简介
+        - chapterList: 章节列表，包含以下字段：
+            - title: 章节标题
+            - url: 章节链接
+    """
+    pass
+
+
+def getChapterContent(url):
+    """
+    :param url: 章节链接
+    :return: 章节内容字符串
+    """
+    pass
+
+if __name__ == '__main__':
+    print(getSiteUrl())
+
